03b3686af4f6705f7443df23bc231732f146211d
igorjobs <igor123106@gmail.com>
2022-03-12 03:18:46 -0300

    com a opcao layout base
    foi atualizado com a opcao de layout base para não precisar repetir codigo

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Ola Mundo teste"
+    
+    nome = "Igor S"
+    dados={"cargo":"CEO","empresa":"@frete.the"}
+    return render_template('index.html', nome=nome,dados=dados) 
+
+@app.route('/contato')
+def contato():
+    
+    nome = "Igor S"
+    dados={"cargo":"CEO","contatointagram":"@frete.the"}
+    return render_template('contato.html', nome=nome,dados=dados)     
